5ae0d91a839213a8d922dbeb9db5e4d20a3d1503
jsuarez1992 <suarez.jessica1992@hotmail.com>
2023-02-27 21:41:59 +0200

    basic tracking log

---
+++ trackingg/Data/IssueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using trackingg.Models;
+
+namespace trackingg.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
